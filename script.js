858a19ea0469c4c8cfdc4687f82876ef3d7f3aa3
unknown <mouatacim.med@gmail.com>
2023-09-28 20:05:35 +0200

    added logic for checking if the name input is empty

--- script.js
+++ script.js
@@ -12,7 +12,9 @@ const displayMoney = (color)=>{
     let value = numInput.value
     let li = document.createElement("li")
     li.textContent = `${nameValue}: ${value}$`
-    if(value != 0){
+    if(value === 0 || nameValue === ""){
+        console.log('error')
+    }else {
         if(color == "red"){
             sum -= Number(value)
         }else{
@@ -23,8 +25,6 @@ const displayMoney = (color)=>{
         let savedBudget = localStorage.getItem("budget")
         budgetDisplay.textContent = `Budget: ${savedBudget}$`
         moneyDisplay.appendChild(li)
-    }else {
-        console.log("error")
     }
 }
 
